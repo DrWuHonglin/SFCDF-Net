89b8bb54ebcab5dd217ccd03b69a3a6cb3650045
AIDL <140626676+DrWuHonglin@users.noreply.github.com>
2025-09-22 17:33:54 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -17,9 +17,9 @@ if __name__ == "__main__":
     x = torch.randn(1, 3, 256, 256)
     y = torch.randn(1, 1, 256, 256)
     net = SFCDFNet(num_class=6)
-    # net.load_state_dict(torch.load('../checkpoint/baseline_resnet50_epoch_33_91.46417727444361'), strict=False)
     flops, params = profile(net, inputs=(x, y))
     macs, params = clever_format([flops, params], "%.3f")
     print("FLOPs:", macs)
     print("params:", params)
+
 
